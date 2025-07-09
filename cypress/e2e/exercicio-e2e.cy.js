524c77cd486ccb76012caec96633c9df4073869b
Vandro <vandro01@yahoo.com.br>
2025-07-09 19:35:18 +0100

    entrega de exercício

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import produtosPage from '../support/page_objects/nome-funcionliada.page';
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
@@ -10,13 +11,70 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
+      produtosPage.visitarUrl()
   });
 
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
+    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
       
-  });
+  cy.fixture('produtos').then(dados => {
+    // Adicionar os 4 produtos
+    for (let i = 0; i < 4; i++) {
+      produtosPage.visitarProduto(dados[i].nomeProduto)
+      produtosPage.adicionarProdutoAoCarrinho(
+        dados[i].tamanho,
+        dados[i].cor,
+        dados[i].quantidade
+      )
+      cy.get('.woocommerce-message').should('contain', dados[i].nomeProduto)
+    }
+
+    // Ir para o carrinho
+    cy.get('.woocommerce-message .button.wc-forward').click()
+
+    // Confirmar página do carrinho
+    cy.url().should('include', 'carrinho')
+
+    // Ir para o checkout
+    cy.get('.checkout-button').click()
+
+    // Preencher formulário de checkout
+    cy.get('#billing_first_name').type('Aline')
+    cy.get('#billing_last_name').type('Santos')
+    cy.get('#billing_address_1').type('Primeiro Endereço')
+    cy.get('#billing_city').type('São Paulo')
+    cy.get('#billing_postcode').type('74684-572')
+    cy.get('#billing_phone').type('119999-9999')
+    cy.get('#billing_email').type('teste@teste.com')
+
+    
+
+    
+    
+   
+    //Finalizar pedido
+        cy.get('#terms').click();
+        cy.get('#place_order').should('be.visible').and('not.be.disabled').click();
+
+        //Validação
+        cy.get('.woocommerce-notice', { timeout: 10000 }).should('contain', 'Obrigado. Seu pedido foi recebido.');
 
 
+
+    // Validar confirmação de pedido
+    cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
+    
 })
+  
+    });
+
+          
+      })
+
+
+
+        
+       
+
+       
+
+  
--- cypress/support/page_objects/nome-funcionliada.page.js
+++ cypress/support/page_objects/nome-funcionliada.page.js
@@ -1,9 +1,93 @@
-class NomeClasse {
+class ProdutosPage {
 
-    nomeMetodo(parametros ){
-        //ações do método
+    visitarUrl(parametros){
+     cy.visit('produtos')   
     }
+
+    buscarProduto(nomeProduto) {
+       cy.get('[name="s"]').eq(1).type(nomeProduto)
+       cy.get('.button-search').eq(1).click()
+    }
+
+    buscarProdutoLista(nomeProduto) {
+        cy.get('.products > .row')
+        .contains(nomeProduto)
+        .click()
+
+    }
+
+    visitarProduto(nomeProduto) {
+       // cy.visit(`produtos/${nomeProduto}`)
+       const urlFormatada = nomeProduto.replace(/ /g, '-')
+       cy.visit(`produtos/${urlFormatada}`)
+
+    }
+
+    adicionarProdutoAoCarrinho(tamanho, cor, quantidade) {
+       // Clica no tamanho
+       cy.get(`.button-variable-item-${tamanho}`, { timeout: 5000 })
+       .should('be.visible')
+       .click()
+
+       // Clica na cor (remove espaços se houver)
+       const corFormatada = cor.replace(/\s/g, '-')
+       cy.get(`.button-variable-item-${corFormatada}`, { timeout: 5000 })
+       .should('be.visible')
+       .click()
+
+       // Define a quantidade
+       cy.get('input.qty')
+       .should('be.visible')
+       .clear()
+       .type(quantidade)
+
+       // Clica no botão de adicionar ao carrinho
+       cy.get('.single_add_to_cart_button')
+       .should('be.visible')
+       .click()
+    }
+
+    irParaOCarrinho() {
+        cy.get('.woocommerce-message .button.wc-forward')
+        .should('be.visible')
+        .click()
+    }
+
+    validarProdutoNoCarrinho(nomeProduto) {
+        cy.get('.woocommerce-cart-form_cart_item')
+        .should('contain' , nomeProduto)
+    }
+
+    validarQuantidadeNoCarrinho(quantidade) {
+        cy.get('.woocommerce-cart-form_cart_item')
+        .find('.qty')
+        .should('contain', quantidade)
+    }
+
+    validarTotalNoCarrinho(total) {
+        cy.get('.order-total')
+        .should('contain', total)
+
+    }
+
+    validarMensagemCarrinho(mensagem) {
+        cy.get('.woocommerce-message')
+        .should('contain' , mensagem)
+    }
+
+    validarCheckout() {
+        cy.get('.woocommerce-notice')
+        .should('contain', 'Obrigado. Seu pedido foi recebido.')
+
+    }
+
+    
+
+    
+
 
 }
 
-export default new NomeClasse()
+   
+
+export default new ProdutosPage()
